0d0f8a56bdecdc87756132eb5e50002f0f3544bc
femas arianda <femasariandarizki@students.undip.ac.id>
2024-04-03 22:29:32 +0700

    halaman pertama dengan vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link :to="{ name: 'product' }">Product</router-link> |
+      <router-link :to="{ name: 'product-detail', params: {id: 123} }">Detail</router-link> |
+      <router-link :to="{ name: 'cart' }">Cart</router-link> |
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style></style>
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Product from "../views/product/ProductIndex.vue";
+import ProductDetail from "../views/product/ProductDetail.vue";
+import Cart from "../views/cart/CartIndex.vue";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "product",
+    component: Product,
+  },
+  {
+    path: "/product/:id",
+    name: "product-detail",
+    component: ProductDetail,
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: Cart,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
