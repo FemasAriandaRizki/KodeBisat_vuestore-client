6b74c6c8843865c80dc062a670690b657e58e63d
femas arianda <femasariandarizki@students.undip.ac.id>
2024-04-04 23:57:17 +0700

    add all files

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,36 @@
   <div id="app">
     <nav>
       <router-link :to="{ name: 'product' }">Product</router-link> |
-      <router-link :to="{ name: 'product-detail', params: {id: 123} }">Detail</router-link> |
-      <router-link :to="{ name: 'cart' }">Cart</router-link> |
+      <router-link :to="{ name: 'product-detail', params: { id: 123 } }"
+        >Detail</router-link
+      >
+      | <router-link :to="{ name: 'cart' }">Cart</router-link> |
     </nav>
     <router-view />
   </div>
 </template>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+  font-family: Arial;
+}
+
+#page-wrap {
+  margin: auto;
+  max-width: 800px;
+  min-height: 100vh;
+}
+
+button {
+  background-color: #41b883;
+  border: none;
+  border-radius: 8px;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  outline: 0;
+  padding: 16px;
+}
+</style>
