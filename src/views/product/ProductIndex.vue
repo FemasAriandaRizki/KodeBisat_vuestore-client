d57c19679faa8316286b86109821625119e6ccb0
femas arianda <femasariandarizki@students.undip.ac.id>
2024-07-09 01:09:46 +0700

    reusable component

--- src/views/product/ProductIndex.vue
+++ src/views/product/ProductIndex.vue
@@ -2,20 +2,12 @@
   <div>
     <div id="page-wrap">
       <class class="grid-wrap">
-        <div 
-          v-for="product in products" 
+        <ProductItem 
+          v-for="product in products"
           :key="product.id"
-          class="product-item"
-        >
-          <img :src="product.imageUrl" alt="" />
-          <h3 class="product-name">{{ product.name }}</h3>
-          <p class="product-price">Rp{{ product.price }}</p>
-          <router-link
-            :to="{ name: 'product-detail', params: { id: product.id } }"
-          >
-            <button>Detail</button>
-          </router-link>
-        </div>
+
+          :product="product"
+        />
       </class>
     </div>
   </div>
@@ -23,8 +15,12 @@
 
 <script>
 import { products } from "../../data-seed";
+import ProductItem from '../../components/ProductItem'
 
 export default {
+  components: {
+    ProductItem
+  },
   data() {
     return {
       products,
@@ -41,33 +37,4 @@ export default {
   margin-top: 16px;
 }
 
-.product-item {
-  align-items: center;
-  border-radius: 8px;
-  box-shadow: 0px 2px 5px #888;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2%;
-  padding: 20px;
-  position: relative;
-  width: 32%;
-}
-
-.product-name {
-  margin-bottom: 0;
-}
-
-img {
-  height: 200px;
-  width: 200px;
-  border-radius: 5%;
-}
-
-a {
-  width: 100%;
-}
-
-button {
-  width: 100%;
-}
 </style>
